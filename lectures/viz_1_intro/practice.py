ffd7f949b831d9bb63168ea689978b35ed9cce25
Charles Huang <chuang2@uchicago.edu>
2024-10-02 21:10:32 -0500

    Update practice.py

--- lectures/viz_1_intro/practice.py
+++ lectures/viz_1_intro/practice.py
@@ -1,7 +1,27 @@
 import pandas as pd
 import altair as alt
+alt.renderers.enable('html')
 
 #installed vega through terminal
 from vega_datasets import data as vega_data
+
+
 cars = vega_data.cars()
-cars.head()
+print(cars.head())
+
+df = pd.DataFrame({
+    'city': ['Seattle', 'Seattle', 'Seattle', 'New York', 'New York', 'New York', 'Chicago', 'Chicago', 'Chicago'],
+    'month': ['Apr', 'Aug', 'Dec', 'Apr', 'Aug', 'Dec', 'Apr', 'Aug', 'Dec'],
+    'precip': [2.68, 0.87, 5.31, 3.94, 4.13, 3.58, 3.62, 3.98, 2.56]
+})
+
+print(df.head())
+
+#chart object
+
+chart = alt.Chart(df).mark_bar().encode(
+    x='month',
+    y='precip',
+    color='city'
+)
+chart.show()
